b78c2d73968800bfa60daf41fcd8c436762cf0b5
valmsmith39a <valmsmith39a@gmail.com>
2018-12-22 20:24:51 -0800

    Anagram

--- general/set1.js
+++ general/set1.js
@@ -26,5 +26,26 @@ function moveZeroes(nums) {
 
 let nums = [0,1,0,3,12]
 // let nums = [0, 0, 1]
+// console.log('move zeros is: ', moveZeroes(nums))
 
-console.log('move zeros is: ', moveZeroes(nums))
+/*
+
+ Given two strings s and t, write a function to determine if t is an anagram of s.
+ Input: s = "anagram", t = "nagaram"
+ Output: true
+
+ Input: s = "rat", t = "car"
+ Output: false
+
+*/
+
+function anagram (s, t) {
+  if (s.length !== t.length) return false
+  return s.split('').sort().join('') === t.split('').sort().join('')
+}
+
+const s = 'anagram'
+const t = 'nagaram'
+// const s = 'rat'
+// const t = 'car'
+console.log(anagram(s, t))
