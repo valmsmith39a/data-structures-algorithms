53b0b808b8b86e6ef3a8ac01ae143117161f0e72
valmsmith39a <valmsmith39a@gmail.com>
2020-12-21 23:35:21 -0800

    remove element

--- general/set1.py
+++ general/set1.py
@@ -1,21 +1,28 @@
 # Find equilibrium index of an array: left and right sums are the same
 
+
 def equilibriumIndex(arr):
-  right = 0
-  leftover = 0
-  equilibIndex = 0
-  total = sum(arr)
-  for i in range(len(arr)-1, -1, -1):
-    leftover = total - (arr[i] + right)
-    right += arr[i]
-    if right == leftover:
-      return i
+    right = 0
+    leftover = 0
+    equilibIndex = 0
+    total = sum(arr)
+    for i in range(len(arr)-1, -1, -1):
+        leftover = total - (arr[i] + right)
+        right += arr[i]
+        if right == leftover:
+            return i
+
 
 arr = [1, 2, 3, 5, 7, 9, 10, -1]
 print(equilibriumIndex(arr))
 
+# Given an array nums and a value val, remove all instances of that value in-place and return the new length.
 
 
-
-
-
+def removeElement(self, nums: List[int], val: int) -> int:
+    count = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[count] = nums[i]
+            count += 1
+    return count
