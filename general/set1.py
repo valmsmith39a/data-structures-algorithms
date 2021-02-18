9733286fc6aaf7a7a7efedc061db4d4f7919a079
valmsmith39a <valmsmith39a@gmail.com>
2021-02-17 22:26:31 -0800

    permutations - approach 1

--- general/set1.py
+++ general/set1.py
@@ -502,3 +502,18 @@ class Node:
 				queue.append(child)
 		# return the final array
 		return array
+
+# permutations
+def getPermutations(array):
+	permutations = []
+	permutationsHelper(array, [], permutations)
+	return permutations
+
+def permutationsHelper(array, currentPermutation, permutations):
+	if not len(array) and len(currentPermutation):
+		permutations.append(currentPermutation)
+	else:
+		for i in range(len(array)):
+			newArray = array[:i] + array[i+1:]
+			newPermutation = currentPermutation + [array[i]]
+			permutationsHelper(newArray, newPermutation, permutations)
