0bee4c3d0c3d987c3dc646e81251694f3aab0b9c
valmsmith39a <valmsmith39a@gmail.com>
2021-03-01 21:32:33 -0800

    binary search tree - in/pre/post order traversals

--- general/set1.py
+++ general/set1.py
@@ -803,3 +803,28 @@ class BinaryTree:
 		self.value = value
 		self.left = None
 		self.right = None
+
+# Binary Search Tree - Traversal 
+
+# O(n) time | O(n) space, if not array accum, then O(d), d is depth of tree 
+def inOrderTraverse(tree, array):
+	if tree is not None:
+		inOrderTraverse(tree.left, array)
+		array.append(tree.value)
+		inOrderTraverse(tree.right, array)
+	return array
+	
+def preOrderTraverse(tree, array):
+	if tree is not None: 
+		array.append(tree.value)
+		preOrderTraverse(tree.left, array)
+		preOrderTraverse(tree.right, array)
+	return array
+
+
+def postOrderTraverse(tree, array):
+	if tree is not None:
+		postOrderTraverse(tree.left, array)
+		postOrderTraerse(tree.right, array)
+		array.append(tree.value)
+	return array
