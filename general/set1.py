8fa71920048c44076e84194263e108ced8a98198
valmsmith39a <valmsmith39a@gmail.com>
2021-03-02 18:36:05 -0800

    binary search - iterative and recursive

--- general/set1.py
+++ general/set1.py
@@ -828,3 +828,40 @@ def postOrderTraverse(tree, array):
 		postOrderTraerse(tree.right, array)
 		array.append(tree.value)
 	return array
+
+
+# Binary Search
+def binarySearch(array, target):
+	return binarySearchHelper(array, target, 0, len(array) - 1)
+
+# iterative solution
+# O(log(n)) time | O(1) space, bc don't store anything 
+def binarySearchHelper(array, target, left, right):
+	while left <= right:
+		middle = (left + right) // 2
+		potentialMatch = array[middle]
+		if target == potentialMatch:
+			return middle
+		elif target < potentialMatch:
+			right = middle - 1
+		else:
+			left = middle + 1 
+			
+	return -1
+	
+# recursive solution
+#  O(log(n) time) | O(log(n)) space, because eliminating half the values with each iteration	
+def binarySearchHelper(array, target, left, right):
+	# left pointer crosses right pointer, didn't find the value
+	if left > right:
+		return -1 
+	middle = (left + right) // 2
+	potentialMatch = array[middle]
+	if target == potentialMatch:
+		return middle
+	elif target < potentialMatch:
+		return binarySearchHelper(array, target, left, middle - 1)
+	else:
+		return binarySearchHelper(array, target, middle + 1, right)
+	
+	
