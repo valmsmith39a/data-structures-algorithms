efa3e122bf54336f63172fa9b7fa7cc0b783fb94
valmsmith39a <valmsmith39a@gmail.com>
2021-03-15 22:25:46 -0700

    merge sort - merge sorted arrays

--- general/review.py
+++ general/review.py
@@ -338,3 +338,24 @@ O(n log n) space
 Solution 2: Sort in place 
 
 '''
+
+def mergeSortedArrays(leftHalf, rightHalf):
+	sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+	k = i = j = 0
+	while i < len(leftHalf) and j < len(rightHalf):
+		if leftHalf[i] < rightHalf[j]:
+			sortedArray[k] = leftHalf[i]
+			i += 1
+		else:
+			sortedArray[k] = rightHalf[j]
+			j += 1
+		k += 1
+	while i < len(leftHalf):
+		sortedArray[k] = leftHalf[i]
+		i += 1
+		k += 1
+	while j < len(rightHalf):
+		sortedArray[k] = rightHalf[j]
+		j += 1
+		k += 1
+	return sortedArray
