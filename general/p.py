ee2f6204ba4f2e98ddb4bf1e890aa105fb6c8a30
valmsmith39a <valmsmith39a@gmail.com>
2021-05-10 21:49:44 -0700

    practice merge sort, quick sort, comments

--- general/p.py
+++ general/p.py
@@ -1,21 +1,29 @@
-
-'''
-O(V + E) time
-O(V) space in the worst case 
-'''
-
-class Node:
-	def __init__(self, name):
-		self.name = name
-		self.children = []
-
-	def breadthFirstSearch(self, array):
-		queue = [self]
-		while len(queue) > 0:
-			current = queue.pop(0)
-			array.append(current.name)
-			for child in current.children:
-				queue.append(child)
+def mergeSort(array):
+	if len(array) == 1: 
 		return array
+	middleIdx = len(array) // 2
+	leftHalf = [:middleIdx]
+	rightHalf = [middleIdx:]
+	return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
 
+def mergeSortedArrays(leftHalf, rightHalf):
+	sortedArray = [None] * (len(leftHalf) + len(rightHalf)) 
+	k = i = j = 0
+	while i < len(leftHalf) and j < len(rightHalf):
+		if leftHalf[i] < rightHalf[j]:
+			sortedArray[k] = leftHalf[i]
+			i += 1
+		else: 
+			sortedArray[k] = rightHalf[j]
+			j += 1
+		k += 1
 
+	while i < len(leftHalf):
+		sortedArray[k] = leftHalf[i]
+		i += 1
+		k += 1
+	while j < len(rightHalf):
+		sortedArray[k] = rightHalf[j]
+		j += 1
+		k += 1
+	return sortedArray
--- general/review.py
+++ general/review.py
@@ -9,15 +9,18 @@ O(n * 2^n) time (exponential time):
     n: iterate through every element in list
     2^n: in 2nd for loop, the list grows
 '''
+
+
 def sub_lists(l):
-	lists = [[]]
-	for i in range(len(l)):
-		orig = lists[:]
-		new = l[i]
-		for j in range(len(lists)):
-			lists[j] = lists[j] + [new]
-		lists = lists + orig
-	return lists
+    lists = [[]]
+    for i in range(len(l)):
+        orig = lists[:]
+        new = l[i]
+        for j in range(len(lists)):
+            lists[j] = lists[j] + [new]
+        lists = lists + orig
+    return lists
+
 
 '''
 2. Basic Hash Table: Two Number Sum
@@ -37,14 +40,16 @@ O(n) time: iterate through entire list
 O(n) space: store n numbers in the hash table
 '''
 
+
 def twoNumberSum(array, targetSum):
-	nums = {}
-	for num in array:
-		potentialMatch = targetSum - num
-		if potentialMatch in nums:
-			return [potentialMatch, num]
-		nums[num] = True
-	return []
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        nums[num] = True
+    return []
+
 
 '''
 3. Binary Search
@@ -72,20 +77,24 @@ Time/Space Complexity:
 O (log n) time: eliminate 1/2 of elements in each iteration
 O (1) space: don't store anything
 '''
+
+
 def binarySearch(array, target):
-	return binarySearchHelper(array, target, 0, len(array) - 1)
+    return binarySearchHelper(array, target, 0, len(array) - 1)
+
 
 def binarySearchHelper(array, target, left, right):
-	if left > right:
-		return -1
-	middle = (left + right) // 2
-	potentialMatch = array[middle]
-	if target == potentialMatch:
-		return middle
-	elif target < potentialMatch:
-		return binarySearchHelper(array, target, left, middle - 1)
-	else: 
-		return binarySearchHelper(array, target, middle + 1, right)
+    if left > right:
+        return -1
+    middle = (left + right) // 2
+    potentialMatch = array[middle]
+    if target == potentialMatch:
+        return middle
+    elif target < potentialMatch:
+        return binarySearchHelper(array, target, left, middle - 1)
+    else:
+        return binarySearchHelper(array, target, middle + 1, right)
+
 
 '''
 4. LinkedList: Reverse a linked list
@@ -99,19 +108,23 @@ def binarySearchHelper(array, target, left, right):
 O(n) time: traverse each node in the list
 O(1) space: no matter how big the list, always only storing previous, current, next nodes 
 '''
+
+
 class LinkedList:
-	def __init__(self, value):
-		self.value = value
-		self.next = None
-		
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 def reverseLinkedList(head):
-	previousNode, currentNode = None, head
-	while currentNode is not None:
-		nextNode = currentNode.next
-		currentNode.next = previousNode
-		previousNode = currentNode
-		currentNode = nextNode
-	return previousNode
+    previousNode, currentNode = None, head
+    while currentNode is not None:
+        nextNode = currentNode.next
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+    return previousNode
+
 
 '''
 5. Stacks: Balanced Brackets
@@ -129,23 +142,26 @@ Key insights:
 O(n) time: iterate through characters in a string
 O(n) space: push characters to a stack 
 '''
+
+
 def balancedBrackets(string):
-	openingBrackets = "([{"
-	closingBrackets = ")]}"
-	matchingBrackets = {")":"(", "]": "[", "}": "{"}
-	stack = []
-	
-	for char in string: 
-		if char in openingBrackets:
-			stack.append(char)
-		elif char in closingBrackets:
-			if len(stack) == 0:
-				return False
-			if stack[-1] == matchingBrackets[char]:
-				stack.pop()
-			else:
-				return False
-	return len(stack) == 0
+    openingBrackets = "([{"
+    closingBrackets = ")]}"
+    matchingBrackets = {")": "(", "]": "[", "}": "{"}
+    stack = []
+
+    for char in string:
+        if char in openingBrackets:
+            stack.append(char)
+        elif char in closingBrackets:
+            if len(stack) == 0:
+                return False
+            if stack[-1] == matchingBrackets[char]:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
+
 
 '''
 6. Graphs: Breadth First Search: 
@@ -161,19 +177,22 @@ O(V + E) time:
 O(V) space: in worst case, queue of child nodes hold V - 1 nodes
 	if all children nodes coonnected to 1 parent, then V - 1 nodes in queue
 '''
+
+
 class Node:
-	def __init__(self, name): 
-		self.name = name
-		self.children = []
-		
-	def breadthFirstSearch(self, array):
-		queue = [self]
-		while len(queue) > 0:
-			current = queue.pop(0)
-			array.append(current.name)
-			for child in current.children:
-				queue.append(child)
-		return array
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def breadthFirstSearch(self, array):
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            array.append(current.name)
+            for child in current.children:
+                queue.append(child)
+        return array
+
 
 '''
 7. Graphs: Depth First Search
@@ -189,26 +208,42 @@ O(V) space: in the worst case, we are adding V frames to the call stack,
 if each node only has one child node and we have a long string of nodes. 
 A - B - C - D - E ... 
 '''
+
+
 class Node:
-	def __init__(self, name):
-		self.name = name
-		self.children = []
-		
-	def depthFirstSearch(self, array):
-		array.append(self.name)
-		for child in self.children:
-			child.depthFirstSearch(array)
-		return array
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
 
 
 '''
 8. Binary Tree: Max Depth
+
+Max Depth: Number of nodes from the root down to the farthest leaf node 
+        1
+    2       3
+4       5
+
+Max Depth is 3 
+
+Depth of node: number of edges from the node to the root node 
+Height of a node: number of edge from the node to the deepest leaf (farthest leaf node)
+
 '''
+
+
 class BinaryTree:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
+
 
 def maxDepth(node):
     if node is None:
@@ -219,8 +254,9 @@ def maxDepth(node):
 
         if lDepth > rDepth:
             return lDepth + 1
-        else: 
+        else:
             return rDepth + 1
+
 
 root = BinaryTree(1)
 root.left = BinaryTree(2)
@@ -240,39 +276,45 @@ O(n) time: traverse through every node in the binary tree and perform constant t
 O(h) space, h = height of binary tree: maxiumum number of function calls on the call stack at one time is h
 For balanced tree, approaches O(log n) because eliminating 1/2 of nodes at each sub tree 
 '''
+
+
 def nodeDepths(root, depth=0):
-	if root is None:
-		return 0
-	return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+    if root is None:
+        return 0
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+
 
 class BinaryTree:
-	def __init__(self, value):
-		self.value = value
-		self.left = None
-		self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
 # iterative solution
 # O(n) time: traverse each node in tree
 # O(h) space: max nodes in stack is roughly height of the tree
+
+
 def nodeDepths(root):
-	# track the sum of depths
-	sumOfDepths = 0
-	# create a stack of nodes to track depths
-	stack = [{"node": root, "depth": 0}]
-	# traverse the binary tree
-	while len(stack) > 0:
-		# pop node off the stack
-		nodeInfo = stack.pop()
-		# get the node, depth
-		node, depth = nodeInfo["node"], nodeInfo["depth"]
-		# account for empty child node
-		if node is None:
-			continue
-		sumOfDepths += depth
-		# push child nodes onto the stack
-		stack.append({"node": node.left, "depth": depth + 1})
-		stack.append({"node": node.right, "depth": depth + 1})
-	return sumOfDepths
+    # track the sum of depths
+    sumOfDepths = 0
+    # create a stack of nodes to track depths
+    stack = [{"node": root, "depth": 0}]
+    # traverse the binary tree
+    while len(stack) > 0:
+        # pop node off the stack
+        nodeInfo = stack.pop()
+        # get the node, depth
+        node, depth = nodeInfo["node"], nodeInfo["depth"]
+        # account for empty child node
+        if node is None:
+            continue
+        sumOfDepths += depth
+        # push child nodes onto the stack
+        stack.append({"node": node.left, "depth": depth + 1})
+        stack.append({"node": node.right, "depth": depth + 1})
+    return sumOfDepths
+
 
 '''
 10. Binary Search Tree: Traversal - in/pre/post order
@@ -292,26 +334,29 @@ Post order:
 O(n) time | O(n) space, if not array accum, then O(d), d is depth of tree 
 '''
 
+
 def inOrderTraverse(tree, array):
-	if tree is not None:
-		inOrderTraverse(tree.left, array)
-		array.append(tree.value)
-		inOrderTraverse(tree.right, array)
-	return array
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
+
 
 def preOrderTraverse(tree, array):
-	if tree is not None:
-		array.append(tree.value)
-		preOrderTraverse(tree.left, array)
-		preOrderTraverse(tree.right, array)
-	return array
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
+    return array
+
 
 def postOrderTraverse(tree, array):
-	if tree is not None:
-		postOrderTraverse(tree.left, array)
-		postOrderTraverse(tree.right, array)
-		array.append(tree.value)
-	return array
+    if tree is not None:
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
 
 
 '''
@@ -347,34 +392,37 @@ Space complexity: O(log n) because run quicksort recursively on the smaller suba
 
 '''
 
+
 def quickSort(array):
-	quickSortHelper(array, 0, len(array) - 1)
-	return array
+    quickSortHelper(array, 0, len(array) - 1)
+    return array
+
 
 def quickSortHelper(array, startIdx, endIdx):
-	if startIdx >= endIdx:
-		return
-	pivotIdx = startIdx
-	leftIdx = startIdx + 1 
-	rightIdx = endIdx
-	while rightIdx >= leftIdx:
-		if array[leftIdx] > array[pivotIdx] and array[rightIdx] < array[pivotIdx]:
-			swap(leftIdx, rightIdx, array)
-		if array[leftIdx] <= array[pivotIdx]:
-			leftIdx += 1
-		if array[rightIdx] >= array[pivotIdx]:
-			rightIdx -= 1
-	swap(pivotIdx, rightIdx, array)
-	leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
-	if leftSubarrayIsSmaller:
-		quickSortHelper(array, startIdx, rightIdx - 1)
-		quickSortHelper(array, rightIdx + 1, endIdx)
-	else:
-		quickSortHelper(array, rightIdx + 1, endIdx)
-		quickSortHelper(array, startIdx, rightIdx - 1)
+    if startIdx >= endIdx:
+        return
+    pivotIdx = startIdx
+    leftIdx = startIdx + 1
+    rightIdx = endIdx
+    while rightIdx >= leftIdx:
+        if array[leftIdx] > array[pivotIdx] and array[rightIdx] < array[pivotIdx]:
+            swap(leftIdx, rightIdx, array)
+        if array[leftIdx] <= array[pivotIdx]:
+            leftIdx += 1
+        if array[rightIdx] >= array[pivotIdx]:
+            rightIdx -= 1
+    swap(pivotIdx, rightIdx, array)
+    leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
+    if leftSubarrayIsSmaller:
+        quickSortHelper(array, startIdx, rightIdx - 1)
+        quickSortHelper(array, rightIdx + 1, endIdx)
+    else:
+        quickSortHelper(array, rightIdx + 1, endIdx)
+        quickSortHelper(array, startIdx, rightIdx - 1)
+
 
 def swap(i, j, array):
-	array[i], array[j] = array[j], array[i]
+    array[i], array[j] = array[j], array[i]
 
 
 '''
@@ -395,32 +443,34 @@ O(n log n) space
 Solution 2: Sort in place
 
 '''
+
+
 def mergeSort(array):
-	if len(array) == 1:
-		return array
-	middleIdx = len(array) // 2
-	leftHalf = array[:middleIdx]
-	rightHalf = array[middleIdx:]
-	return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+    if len(array) == 1:
+        return array
+    middleIdx = len(array) // 2
+    leftHalf = array[:middleIdx]
+    rightHalf = array[middleIdx:]
+    return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+
 
 def mergeSortedArrays(leftHalf, rightHalf):
-	sortedArray = [None] * (len(leftHalf) + len(rightHalf))
-	k = i = j = 0
-	while i < len(leftHalf) and j < len(rightHalf):
-		if leftHalf[i] < rightHalf[j]:
-			sortedArray[k] = leftHalf[i]
-			i += 1
-		else:
-			sortedArray[k] = rightHalf[j]
-			j += 1
-		k += 1
-	while i < len(leftHalf):
-		sortedArray[k] = leftHalf[i]
-		i += 1
-		k += 1
-	while j < len(rightHalf):
-		sortedArray[k] = rightHalf[j]
-		j += 1
-		k += 1
-	return sortedArray
-    
+    sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+    k = i = j = 0
+    while i < len(leftHalf) and j < len(rightHalf):
+        if leftHalf[i] < rightHalf[j]:
+            sortedArray[k] = leftHalf[i]
+            i += 1
+        else:
+            sortedArray[k] = rightHalf[j]
+            j += 1
+        k += 1
+    while i < len(leftHalf):
+        sortedArray[k] = leftHalf[i]
+        i += 1
+        k += 1
+    while j < len(rightHalf):
+        sortedArray[k] = rightHalf[j]
+        j += 1
+        k += 1
+    return sortedArray
