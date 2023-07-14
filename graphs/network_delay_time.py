75ee5249ccb58e792e14a2fdfa999fb7f9141a0b
Xian Wee <valmsmith39a@gmail.com>
2023-07-13 22:03:14 -0700

    Network Delay Time comments

--- graphs/network_delay_time.py
+++ graphs/network_delay_time.py
@@ -1,7 +1,24 @@
+import heapq
+import collections 
+from typing import List
 
 class NetworkDelayTime:
+    """
+    Problem: Network Delay Time (LeetCode #743) 
+    
+    Signal is sent from a given node. Get the min time it takes all nodes to receive the signal. 
 
-    def network_delay_time(self, times: List[List[int]], n: int, k: int) -> int: 
+    Key Insights: 
+    1. Use Dijkstra's algorithm to get the shortest path from start node to every other node in the graph 
+
+    Time Complexity: O(E log E): Each edge is added to the heap once and removed once. Each heap push / pop takes O (log E) time. 
+    Space Complexity: O(E + V), E = number of edges, V = number of vertices (nodes)
+        1. edges: O(E)
+        2. min heap: O(V)
+        3. visited: O(V)
+    """
+
+    def network_delay_time(self, times: List[List[int]], n: int, k: int) -> int:
         edges = collections.defaultdict(list)
 
         for u, v, w in times:
@@ -14,12 +31,13 @@ class NetworkDelayTime:
         while min_heap:
             w1, n1 = heapq.heappop(min_heap)
             if n1 in visited:
-                continue
+                continue 
             visited.add(n1)
             t = max(t, w1)
 
             for n2, w2 in edges[n1]:
                 if n2 not in visited:
                     heapq.heappush(min_heap, (w1 + w2, n2))
-
+        
         return t if len(visited) == n else -1 
+
