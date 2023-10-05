2497c0684042354e247c2a832266c55789ed9d11
Xian Wee <valmsmith39a@gmail.com>
2023-10-04 21:02:20 -0700

    Word Ladder

---
+++ graphs/word_ladder.py
@@ -0,0 +1,43 @@
+import collections 
+from collections import deque
+
+class WordLadder: 
+    """
+    1. Problem: Word Ladder (#127) 
+    
+    Key Insights: 
+    1. Breadth First Search 
+    2. Use wildcard * to build the adjancy list 
+
+    Time Complexity: O(m^2 * n), m: length of each word, n: number of words
+    Space Complexity: O(m^2 * n)
+    """
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0 
+        nei = collections.defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j + 1:]
+                nei[pattern].append(word)
+                
+        visit = set([beginWord])
+        q = deque([beginWord])
+        res = 1
+        
+        while q: 
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res 
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res += 1 
+
+        return 0
